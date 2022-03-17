b7d1a0fe2e5accacf934a2dd3498682eee9db2b3
GitHub Action <action@github.com>
2022-03-17 00:32:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zettianmaximus/newckmetrics/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zettianmaximus.newckmetrics.services
+
+import com.github.zettianmaximus.newckmetrics.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
